b218d555ef7a7540f72167acdf8f3b67058a329f
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-05 12:38:13 +0200

    Mi primer comit

---
+++ Unidad2/src/ComentarioLinea.java
@@ -0,0 +1,10 @@
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		//System.out.println("Hola");
+        System.out.println("Adios");
+        System.out.println("Fin de Programa");
+	}
+
+}
